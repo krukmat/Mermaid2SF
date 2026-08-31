6c7c170faeabb8dc3e1a2a5f6f638f282975a63d
Matias Kruk <kruk.matias@gmail.com>
2026-08-31 10:22:15 +0200

    feat: serialize typed update-record values

--- src/generators/xml/strategies/record-update-strategy.ts
+++ src/generators/xml/strategies/record-update-strategy.ts
@@ -1,47 +1,40 @@
 import { RecordUpdateElement } from '../../../types/flow-dsl';
+import { serializeFlowValueXml } from '../../../types/flow-value';
 import { ElementStrategy, XMLGeneratorContext } from './element-strategy';
 
 export class RecordUpdateStrategy implements ElementStrategy<RecordUpdateElement> {
   generate(element: RecordUpdateElement, context: XMLGeneratorContext): string[] {
     const lines: string[] = [];
     const apiName = element.apiName || element.id;
-
-    lines.push(`    <recordUpdates>`);
+    lines.push('    <recordUpdates>');
     lines.push(`        <name>${apiName}</name>`);
     lines.push(`        <label>${context.escapeXml(element.label || apiName)}</label>`);
-    lines.push(`        <locationX>0</locationX>`);
-    lines.push(`        <locationY>0</locationY>`);
-    lines.push(`        <object>${element.object}</object>`);
+    lines.push('        <locationX>0</locationX>');
+    lines.push('        <locationY>0</locationY>');
 
-    if (element.filters && element.filters.length > 0) {
-      for (const filter of element.filters) {
-        lines.push(`        <filters>`);
-        lines.push(`            <field>${filter.field}</field>`);
-        lines.push(`            <operator>${filter.operator}</operator>`);
-        lines.push(`            <value>`);
-        lines.push(`                <stringValue>${context.escapeXml(filter.value)}</stringValue>`);
-        lines.push(`            </value>`);
-        lines.push(`        </filters>`);
-      }
+    if (element.filterLogic) lines.push(`        <filterLogic>${context.escapeXml(element.filterLogic)}</filterLogic>`);
+    for (const filter of element.filters || []) {
+      lines.push('        <filters>');
+      lines.push(`            <field>${context.escapeXml(filter.field)}</field>`);
+      lines.push(`            <operator>${filter.operator}</operator>`);
+      lines.push('            <value>');
+      lines.push(...serializeFlowValueXml(filter.value, context.escapeXml, 16));
+      lines.push('            </value>');
+      lines.push('        </filters>');
     }
 
-    const sortedFields = Object.keys(element.fields).sort();
-    for (const fieldName of sortedFields) {
-      lines.push(`        <inputAssignments>`);
-      lines.push(`            <field>${fieldName}</field>`);
-      lines.push(`            <value>`);
-      lines.push(
-        `                <stringValue>${context.escapeXml(element.fields[fieldName])}</stringValue>`,
-      );
-      lines.push(`            </value>`);
-      lines.push(`        </inputAssignments>`);
+    for (const fieldName of Object.keys(element.fields).sort()) {
+      lines.push('        <inputAssignments>');
+      lines.push(`            <field>${context.escapeXml(fieldName)}</field>`);
+      lines.push('            <value>');
+      lines.push(...serializeFlowValueXml(element.fields[fieldName], context.escapeXml, 16));
+      lines.push('            </value>');
+      lines.push('        </inputAssignments>');
     }
 
-    if (element.next) {
-      lines.push(...context.generateConnectorLines(element.next, 8));
-    }
-
-    lines.push(`    </recordUpdates>`);
+    if (element.next) lines.push(...context.generateConnectorLines(element.next, 8));
+    lines.push(`        <object>${context.escapeXml(element.object)}</object>`);
+    lines.push('    </recordUpdates>');
     return lines;
   }
 }
