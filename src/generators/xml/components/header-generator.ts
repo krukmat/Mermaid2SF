a05ccf2d8b2a84f0de302e41e1752c90e096f3a4
Matias Kruk <kruk.matias@gmail.com>
2026-08-31 10:17:56 +0200

    fix: map Flow kind to Salesforce processType

--- src/generators/xml/components/header-generator.ts
+++ src/generators/xml/components/header-generator.ts
@@ -1,13 +1,15 @@
-import { FlowDSL } from '../../../types/flow-dsl';
+import { FlowDSL, DEFAULT_API_VERSION, resolveFlowKind } from '../../../types/flow-dsl';
+import { toSalesforceProcessType } from '../../../types/flow-kind';
 
 export class HeaderGenerator {
   generate(dsl: FlowDSL, escapeXml: (text: string) => string): string[] {
+    const processType = toSalesforceProcessType(resolveFlowKind(dsl));
     return [
       '<?xml version="1.0" encoding="UTF-8"?>',
       '<Flow xmlns="http://soap.sforce.com/2006/04/metadata">',
-      `    <apiVersion>${dsl.apiVersion || '60.0'}</apiVersion>`,
+      `    <apiVersion>${dsl.apiVersion || DEFAULT_API_VERSION}</apiVersion>`,
       `    <label>${escapeXml(dsl.label)}</label>`,
-      `    <processType>${dsl.processType}</processType>`,
+      `    <processType>${processType}</processType>`,
     ];
   }
 }
